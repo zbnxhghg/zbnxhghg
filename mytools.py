db85a0d903cc66ae4316e1aff69aa9df85e96cc4
zbnxhghg <2972094103@qq.com>
2023-11-23 10:17:13 +0800

    auto

--- mytools.py
+++ mytools.py
@@ -29,6 +29,14 @@ def 相关系数判断(系数:int):
      """
      判断相关系数的强弱
 
+     判断依据：
+     区间    强弱
+     0.8-1.0 极强相关
+     0.6-0.8 强相关
+     0.4-0.6 中等强度相关
+     0.2-0.4 弱相关
+     0.0-0.2 极弱相关或无相关
+
      """
      if 系数 >= 0.8:
              return '极强相关'
@@ -42,3 +50,34 @@ def 相关系数判断(系数:int):
              return '极弱相关或无相关'
      
 
+def goodmanKruska_tau_y(df, x: str, y: str) -> float:
+    """ 计算两个定类变量的goodmanKruska_tau_y相关系数
+    
+    df:包含定类变量的数据框
+    x: 数据框中作为自变量的定类变量名称
+    y: 数据框中作为因变量的定类变量名称
+    
+    函数返回tau_y相关系数
+    """
+
+
+    cft = pd.crosstab(df[y], df[x], margins=True)
+    """ 取得全部个案数目 """
+    n = cft.at['All', 'All']
+    """ 初始化变量 """
+    E_1 = E_2 = tau_y = 0
+
+    """ 计算E_1 """
+    for i in range(cft.shape[0] - 1):
+        F_y = cft['All'][i]
+        E_1 += ((n - F_y) * F_y) / n
+    """ 计算E_2 """
+    for j in range(cft.shape[1] - 1):
+        for k in range(cft.shape[0] - 1):
+            F_x = cft.iloc[cft.shape[0] - 1, j]
+            f = cft.iloc[k, j]
+            E_2 += ((F_x - f) * f) / F_x
+    """ 计算tauy """
+    tau_y = (E_1 - E_2) / E_1
+
+    return tau_y
