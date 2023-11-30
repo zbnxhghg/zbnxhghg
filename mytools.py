f83a5f476a2ec2f03f87d558bb3e4f9286c3c675
zbnxhghg <2972094103@qq.com>
2023-11-30 20:49:50 +0800

    auto

--- mytools.py
+++ mytools.py
@@ -4,20 +4,20 @@ import matplotlib.pyplot as plt
 plt.rcParams["font.sans-serif"]=["SimHer"]#设置字体
 
 
-def 读取spss数据文件(文件位置信息,是否保留标签值:bool):
-        数据表,metadata = pyreadstat.read_sav(数据位置及名称,apply_value_formats=True,format_as_ordered_category=True)
-        return 数据表
+def 读取SPSS数据文件(文件位置及名称, 是否保留标签值: bool):
+    数据表, metadata = pyreadstat.read_sav(
+        文件位置及名称, apply_value_formats=是否保留标签值,
+        formats_as_ordered_category=True)
+    return 数据表
 
-def 生成有序类别变量描述统计表(表名,变量名):
-        
-                     result = 表名[变量名].value_counts(sort=False)
-                     描述统计表 = pd.DataFrame(result)
-                     描述统计表['比例']=描述统计表['count']/描述统计表['count'].sum()
-                     描述统计表['累计求和'] = df_result['count'].cumsum()
-                     df_result['比例'] = 描述统计表['比例'].cumsum()
-                     return 描述统计表
+def 有序变量描述统计函数(表名, 变量名):
+    result = 表名[变量名].value_counts(sort=False)
+    描述统计表 = pd.DataFrame(result)
+    描述统计表['比例'] = 描述统计表['count'].value_counts(normalize=True)
+    描述统计表['累计比例'] = 描述统计表['比例'].cumsum()
+    return 描述统计表
 
-def 绘制直方图(表名,y):
+def 绘制柱状图(表名,y):
         x = 表名.index
         y = 表名[y].values
         fig,ax2 = plt.subplot()
@@ -87,3 +87,5 @@ def 有序变量描述统计函数(表名, 变量名):
     描述统计表['比例'] = 描述统计表['count'].value_counts(normalize=True)
     描述统计表['累计比例'] = 描述统计表['比例'].cumsum()
     return 描述统计表
+
+
